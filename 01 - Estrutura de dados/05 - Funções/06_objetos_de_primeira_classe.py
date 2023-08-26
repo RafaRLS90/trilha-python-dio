a96deab61e0e5c287499e5118790649d2e74b9a4
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-08-26 11:20:15 -0300

    praticando funções

--- 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
+++ 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
@@ -1,10 +1,18 @@
 def somar(a, b):
     return a + b
 
+def subtrair(a, b):
+    return a- b
+
 
 def exibir_resultado(a, b, funcao):
     resultado = funcao(a, b)
     print(f"O resultado da operação {a} + {b} = {resultado}")
 
+def exibir_resultado2(a, b, funcao2):
+    resultado2 = funcao2(a, b)
+    print(f"O resultado da operação {a} - {b} = {resultado2}")
+
 
 exibir_resultado(10, 10, somar)  # O resultado da operação 10 + 10 = 20
+exibir_resultado2(10, 10, subtrair)
